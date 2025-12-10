e4517ed25074f513be95a96b9107d0a3e618aff7
aditya07230 <adityakatore249@gmail.com>
2025-12-10 17:58:12 +0530

    Admin.java is Modified

--- Admin.java
+++ Admin.java
@@ -1,3 +1,25 @@
-class Admin{
-    
+import javax.print.DocFlavor.STRING;
+
+class Admin {
+    private String adminId;
+    private String name;
+    public String email;
+    private String password;
+
+    public Admin(String adminId, String name, String email, String password) {
+        this.adminId = adminId;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+    public String getAdminID(){
+        return adminId;
+    }
+    public String getPassword() {
+     return password; 
+ }
+    public String getEmail() {
+     return email; 
+ }
+
 }
