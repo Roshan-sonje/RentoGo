e84c893b0856411b567a1edf1ad7a0c5e41cbb70
Roshan Sonje <91359767+Roshan-sonje@users.noreply.github.com>
2025-12-12 18:41:29 +0530

    Refactor Booking class with constructor and methods

--- Booking.java
+++ Booking.java
@@ -1,3 +1,30 @@
-public class Booking {
+class Booking{
+	String bookingId;
+	String userEmail;
+	Payment payment;
 
+	Booking(String bookingId, String userEmail,Payment payment){
+		this.bookingId = bookingId;
+		this.userEmail = userEmail;
+		this.payment = payment;
+	}
+
+	public String getBookingId(){
+		return this.bookingId;
+	}
+	public String getUserEmail(){
+		return this.userEmail;
+	}
+	public void setBookingId(String bookingId){
+		this.bookingId = bookingId;
+	}
+	public void setUserEmail(String userEmail){
+		this.userEmail = userEmail;
+	}
+	public void getBookingInfo(){
+		System.out.println("\n*** Booking Info ***\n");
+		System.out.println("Booking Id : " + bookingId);
+		System.out.println("User Email : " + userEmail);
+		payment.getPaymentInfo();
+	}
 }
